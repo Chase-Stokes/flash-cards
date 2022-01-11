4cfdc10d20209a741958293678e0182af3093f12
Liam Eller <liamthelastson@gmail.com>
2022-01-11 15:09:14 -0800

    WIP: add timed removeClass function to divs.
    co-authored-by: Riley Shimp <rilryshimp@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
-  $(".card").click(function() {
-    $("#card-1").toggle();
+  $("#card-1").click(function() {
+    $("#card-1").addClass("inset");
+    $("#card-1").removeClass("inset");
+    $("#p-1").toggle();
   });
-  $(".card").click(function() {
-    $("#card-2").toggle();
+  $("#card-2").click(function() {
+    $("#p-2").toggle();
   });
-  $(".card").click(function() {
-    $("#card-3").toggle();
+  $("#card-3").click(function() {
+    $("#p-3").toggle();
   });
-  $(".card").click(function() {
-    $("#card-4").toggle();
+  $("#card-4").click(function() {
+    $("#p-4").toggle();
   });
-  $(".card").click(function() {
-    $("#card-5").toggle();
+  $("#card-5").click(function() {
+    $("#p-5").toggle();
   });
   $("#card-6").click(function() {
     $("#p-6").toggle();
