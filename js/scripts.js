4b6522cbe3b985a8216792f0adde54728c220fde
Liam Eller <liamthelastson@gmail.com>
2022-01-11 14:45:59 -0800

    add hhtml with flash cards, custom styling and toggle capability
    co-authored-by: Riley Shimp <rilryshimp@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $(".card").click(function() {
+    $("#card-1").toggle();
+  });
+  $(".card").click(function() {
+    $("#card-2").toggle();
+  });
+  $(".card").click(function() {
+    $("#card-3").toggle();
+  });
+  $(".card").click(function() {
+    $("#card-4").toggle();
+  });
+  $(".card").click(function() {
+    $("#card-5").toggle();
+  });
+  $("#card-6").click(function() {
+    $("#p-6").toggle();
+  });
+});
