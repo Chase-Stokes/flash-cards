c60dbe554ec78e0af2d8f9cc4540fedd65017200
Liam Eller <liamthelastson@gmail.com>
2022-01-11 15:28:52 -0800

    add setTimeOut to flashcards
    co-authored-by: Riley Shimp <rilryshimp@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,44 @@
 $(document).ready(function() {
   $("#card-1").click(function() {
-    $("#card-1").addClass("inset");
-    $("#card-1").removeClass("inset");
     $("#p-1").toggle();
+    $("#card-1").addClass("inset");
+    setTimeout(function(){
+      $("#card-1").removeClass("inset");
+    }, 125);
   });
   $("#card-2").click(function() {
     $("#p-2").toggle();
+    $("#card-2").addClass("inset");
+    setTimeout(function(){
+      $("#card-2").removeClass("inset");
+    }, 125);
   });
   $("#card-3").click(function() {
     $("#p-3").toggle();
+    $("#card-3").addClass("inset");
+    setTimeout(function(){
+      $("#card-3").removeClass("inset");
+    }, 125);
   });
   $("#card-4").click(function() {
     $("#p-4").toggle();
+    $("#card-4").addClass("inset");
+    setTimeout(function(){
+      $("#card-4").removeClass("inset");
+    }, 125);
   });
   $("#card-5").click(function() {
     $("#p-5").toggle();
+    $("#card-5").addClass("inset");
+    setTimeout(function(){
+      $("#card-5").removeClass("inset");
+    }, 125);
   });
   $("#card-6").click(function() {
     $("#p-6").toggle();
+    $("#card-6").addClass("inset");
+    setTimeout(function(){
+      $("#card-6").removeClass("inset");
+    }, 125);
   });
 });
