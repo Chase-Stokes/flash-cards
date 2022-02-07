cd54beca54a9d0fb97bcc1a9c5fe99f2fa5c9256
Liam Eller <liamthelastson@gmail.com>
2022-02-07 14:25:54 -0800

    Aadd event listeners
    Co-Authored-by: Chase Stokes <chasehstokes@gmail.com>
    Co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,48 +2,23 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Trivia from './js/scripts.js';
 
 $(document).ready(function() {
-  $("#card-1").click(function() {
-    $("#p-1").toggle();
-    $("#card-1").addClass("inset");
-    setTimeout(function(){
-      $("#card-1").removeClass("inset");
-    }, 125);
-  });
-  $("#card-2").click(function() {
-    $("#p-2").toggle();
-    $("#card-2").addClass("inset");
-    setTimeout(function(){
-      $("#card-2").removeClass("inset");
-    }, 125);
-  });
-  $("#card-3").click(function() {
-    $("#p-3").toggle();
-    $("#card-3").addClass("inset");
-    setTimeout(function(){
-      $("#card-3").removeClass("inset");
-    }, 125);
-  });
-  $("#card-4").click(function() {
-    $("#p-4").toggle();
-    $("#card-4").addClass("inset");
-    setTimeout(function(){
-      $("#card-4").removeClass("inset");
-    }, 125);
-  });
-  $("#card-5").click(function() {
-    $("#p-5").toggle();
-    $("#card-5").addClass("inset");
-    setTimeout(function(){
-      $("#card-5").removeClass("inset");
-    }, 125);
-  });
-  $("#card-6").click(function() {
-    $("#p-6").toggle();
-    $("#card-6").addClass("inset");
-    setTimeout(function(){
-      $("#card-6").removeClass("inset");
-    }, 125);
-  });
+
+  Trivia.triviaCards();
+
+  for (let i = 0; i < 7; i++) {
+    $(".q"+i+"-btn").click(function() {
+      $(".flip-card"+i).toggleClass('flipped');
+      let answer = $(".answer"+i).text();
+      let radio = $("input:radio[name=q"+i+"]:checked").val();
+      if (radio === answer) {
+        $("#result" + i).text("You are correct!");
+        // $(".flip-card"+i).hide();
+      } else {
+        $("#result" + i).text("You are wrong..");
+      }
+    });
+  }
 });
